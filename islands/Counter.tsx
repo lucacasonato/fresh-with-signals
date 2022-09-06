bb3036c2e5fe665e262c33b9cb92667995c5ac06
Luca Casonato <hello@lcas.dev>
2022-09-06 20:31:13 +0200

    Switch to signals

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,4 +1,4 @@
-import { useState } from "preact/hooks";
+import { useSignal } from "@preact/signals";
 import { Button } from "../components/Button.tsx";
 
 interface CounterProps {
@@ -6,12 +6,12 @@ interface CounterProps {
 }
 
 export default function Counter(props: CounterProps) {
-  const [count, setCount] = useState(props.start);
+  const count = useSignal(props.start);
   return (
     <div class="flex gap-2 w-full">
       <p class="flex-grow-1 font-bold text-xl">{count}</p>
-      <Button onClick={() => setCount(count - 1)}>-1</Button>
-      <Button onClick={() => setCount(count + 1)}>+1</Button>
+      <Button onClick={() => count.value--}>-1</Button>
+      <Button onClick={() => count.value++}>+1</Button>
     </div>
   );
 }
